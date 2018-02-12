e6b4f014ca8f06e18429ae2500cf1c1613b78b93
jf.gutierrez13 <jf.gutierrez13@ISIS2603S2-0070.sis.virtual.uniandes.edu.co>
2018-02-12 08:57:47 -0500

    Demas DTOs, falta agregar las relaciones con los dtos faltantes

---
+++ turismo-web/src/main/java/co/edu/uniandes/csw/turismo/dtos/ValoracionesDetailDTO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.turismo.dtos;
+
+/**
+ *
+ * @author jf.gutierrez13
+ */
+public class ValoracionesDetailDTO extends ValoracionesDTO
+{
+    /**
+     * Constructor por defecto
+     */
+    public ValoracionesDetailDTO()
+    {
+        
+    }
+    
+    
+    
+}
