0586f210cd8e90edcd648fb7b940b3a29a047908
SEBASTIAN BENITEZ DIAZ <s.benitez10@sis.virtual.uniandes.edu.co>
2018-02-14 19:33:25 -0500

    Documentacion de DTOS y Resource de Factura y tarjeta de credito

--- turismo-web/src/main/java/co/edu/uniandes/csw/turismo/dtos/TarjetaDeCreditoDTO.java
+++ turismo-web/src/main/java/co/edu/uniandes/csw/turismo/dtos/TarjetaDeCreditoDTO.java
@@ -6,7 +6,29 @@
 package co.edu.uniandes.csw.turismo.dtos;
 
 /**
+ ** TarjetaDeCreditoDTO Objeto de transferencia de datos de tarjetas de credito. Los DTO contienen las
+ * represnetaciones de los JSON que se transfieren entre el cliente y el
+ * servidor.
+ * 
+ * Al serializarse como JSON esta clase implementa el siguiente modelo: <br>
+ * <pre>
+ *   {
+ *      "name": String,
+ *      "numero": number,
+ *      "CDV":numero
+ *   }
+ * </pre>
+ * Por ejemplo una tarjeta de credito se representa asi:<br>
+ * 
+ * <pre>
+ * 
+ *   {
+ *      "name": Sebastian,
+ *      "numero": 1231415,
+ *      "CDV":321
+ *   }
  *
+ * </pre>
  * @author s.benitez10
  */
 public class TarjetaDeCreditoDTO 
@@ -14,7 +36,9 @@ public class TarjetaDeCreditoDTO
     private String name;
     private long  numero;
     private int CVD;
-    
+     /*
+    Constructor por defecto
+    */
 
     public TarjetaDeCreditoDTO(){
         
@@ -26,27 +50,39 @@ public class TarjetaDeCreditoDTO
         this.CVD = CVD;
        
     }
-
+/*
+    retorna el nombre del dueño de la tarjeta
+    */
     public String getName() {
         return name;
     }
-
+/*
+    retorna el nombre del dueño de la tarjeta
+    */
     public void setName(String name) {
         this.name = name;
     }
-
+/*
+    retorna el numero de la tarjeta
+    */
     public long getNumero() {
         return numero;
     }
-
+/*
+    modifica el numero de la tarjeta
+    */
     public void setNumero(long numero) {
         this.numero = numero;
     }
-
+/*
+    retorna el codigo CVD de la tarjeta
+    */
     public int getCVD() {
         return CVD;
     }
-
+/*
+    modeifica el CVD de la tarjeta
+    */
     public void setCVD(int CVD) {
         this.CVD = CVD;
     }
--- turismo-web/src/main/java/co/edu/uniandes/csw/turismo/resources/FacturaResource.java
+++ turismo-web/src/main/java/co/edu/uniandes/csw/turismo/resources/FacturaResource.java
@@ -20,8 +20,20 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 /**
+ ** <pre>Clase que implementa el recurso "facturas".
+ * URL: /api/facturas
+ * </pre>
+ * <i>Note que la aplicación (definida en {@link RestConfig}) define la ruta "/api" y
+ * este recurso tiene la ruta "facturas".</i>
  *
+ * <h2>Anotaciones </h2>
+ * <pre>
+ * Path: indica la dirección después de "api" para acceder al recurso
+ * Produces/Consumes: indica que los servicios definidos en este recurso reciben y devuelven objetos en formato JSON
+ * RequestScoped: Inicia una transacción desde el llamado de cada método (servicio). 
+ * </pre>
  * @author s.benitez10
+ * @version 1.0
  */
 @Path("facturas")
 @Produces("application/json")
@@ -29,27 +41,107 @@ import javax.ws.rs.Produces;
 @RequestScoped
 public class FacturaResource
 {
+      /**
+     * <h1>POST /api/facturas : Crear una factura.</h1>
+     * 
+     * <pre>Cuerpo de petición: JSON {@link FacturaDetailDTO}.
+     * 
+     * Crea una nueva factura con la informacion que se recibe en el cuerpo 
+     * de la petición y se regresa un objeto identico con un id auto-generado 
+     * por la base de datos.
+     * 
+     * Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Creó la nueva factura.
+     * </code>
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 412 Precodition Failed: Ya existe la ciudad.
+     * </code>
+     * </pre>
+     * @param factura {@link FacturaDetailDTO} - La factura que se desea guardar.
+     * @return JSON {@link FacturaDetailDTO}  - La factura guardada con el atributo id autogenerado.
+     * @throws BusinessLogicException {@link BusinessLogicExceptionMapper} - Error de lógica que se genera cuando ya existe la ciudad.
+     */
    @POST
     public FacturaDetailDTO createFactura(FacturaDetailDTO factura) throws BusinessLogicException {
         return factura;
     }  
     
+    /**
+     * <h1>GET /api/facturas: Obtener todas las facturas.</h1>
+     * 
+     * <pre>Busca y devuelve todas las facturas que existen en la aplicacion.
+     * 
+     * Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Devuelve todas las facturas de la aplicacion.</code> 
+     * </pre>
+     * @return JSONArray {@link FacturasDetailDTO} - Las facturas encontradas en la aplicación. Si no hay ninguna retorna una lista vacía.
+     */
     @GET
     public List<FacturaDetailDTO> getFactura() {
         return new ArrayList<>();
     }
     
+     /**
+     * <h1>GET /api/facturas/{id} : Obtener factura por id.</h1>
+     * 
+     * <pre>Busca la factura con el id asociado recibido en la URL y la devuelve.
+     * 
+     * Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Devuelve la factura correspondiente al id.
+     * </code> 
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 404 Not Found No existe una factura con el id dado.
+     * </code> 
+     * </pre>
+     * @param id Identificador de la factura que se esta buscando. Este debe ser una cadena de dígitos.
+     * @return JSON {@link FacturaDetailDTO} - La factura buscada
+     */
     @GET
     @Path("{id: \\d+}")
     public FacturaDetailDTO getFactura(@PathParam("id") Long id) {
         return null;
     }
+    /**
+     * <h1>PUT /api/facturas/{id} : Actualizar factura con el id dado.</h1>
+     * <pre>Cuerpo de petición: JSON {@link FacturaDetailDTO}.
+     * 
+     * Actualiza la factura con el id recibido en la URL con la informacion que se recibe en el cuerpo de la petición.
+     * 
+     * Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Actualiza la factura con el id dado con la información enviada como parámetro. Retorna un objeto identico.</code> 
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 404 Not Found. No existe una factura con el id dado.
+     * </code> 
+     * </pre>
+     * @param id Identificador de la factura que se desea actualizar.Este debe ser una cadena de dígitos.
+     * @param Factura {@link FacturaDetailDTO} La factura que se desea guardar.
+     * @return JSON {@link FacturaDetailDTO} - La factura guardada.
+     * @throws BusinessLogicException {@link BusinessLogicExceptionMapper} - Error de lógica que se genera al no poder actualizar la factura porque ya existe una con ese nombre.
+     */
     
      @PUT
     @Path("{id: \\d+}")
     public FacturaDetailDTO updateFactura(@PathParam("id") Long id, FacturaDetailDTO Factura) throws BusinessLogicException {
         return Factura;
     }
+     /**
+     * <h1>DELETE /api/facturas/{id} : Borrar factura por id.</h1>
+     * 
+     * <pre>Borra la factura con el id asociado recibido en la URL.
+     * 
+     * Códigos de respuesta:<br>
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Elimina la factura correspondiente al id dado.</code>
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 404 Not Found. No existe una factura con el id dado.
+     * </code>
+     * </pre>
+     * @param id Identificador de la factura que se desea borrar. Este debe ser una cadena de dígitos.
+     */
     
     @DELETE
     @Path("{id: \\d+}")
