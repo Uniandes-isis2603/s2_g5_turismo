4d8180adda18048a401dd949ff4f05ad140f5c02
lf.rivera10 <lf.rivera10@ISIS2603S2-0078.sis.virtual.uniandes.edu.co>
2018-02-26 09:38:41 -0500

    cascade

---
+++ turismo-back/src/main/java/co/edu/uniandes/csw/turismo/entities/ComentarioEntity.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.turismo.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author lf.rivera10
+ */
+@Entity
+public class ComentarioEntity extends BaseEntity implements Serializable {
+    
+    private String comentario;
+   
+
+    /**
+     * @return El contenido del comentario
+     */
+    public String getComentario() {
+        return comentario;
+    }
+ /**
+     * @param comentario El nuevo contenido del comentario 
+     */
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+    
+    
+}
