da5aa559d895d71b36e497e92324f720e22319b8
lf.rivera10 <lf.rivera10@ISIS2603S2-0078.sis.virtual.uniandes.edu.co>
2018-02-26 10:00:28 -0500

    cascade

---
+++ turismo-back/src/main/java/co/edu/uniandes/csw/turismo/ejb/ComentarioLogic.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.turismo.ejb;
+
+import co.edu.uniandes.csw.turismo.entities.ComentarioEntity;
+import co.edu.uniandes.csw.turismo.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.turismo.persistence.ComentarioPersistence;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author lf.rivera10
+ */
+@Stateless
+public class ComentarioLogic {
+    private static final Logger LOGGER = Logger.getLogger(ComentarioLogic.class.getName());
+     
+    
+     @Inject
+    private ComentarioPersistence persistence;
+    
+     
+     public ComentarioEntity createComentario(ComentarioEntity entity) throws BusinessLogicException {
+        LOGGER.info("Inicia proceso de creación de Comentario");
+       
+        if (persistence.find(entity.getId()) != null) 
+        {
+            throw new BusinessLogicException("Ya existe este Comentario");
+        }
+        // Invoca la persistencia para crear el Comentario
+        persistence.create(entity);
+        LOGGER.info("Termina proceso de creación Comentarios");
+        return entity;
+    }
+     
+      public List<ComentarioEntity> getComentarios() {
+        LOGGER.info("Inicia proceso de consultar todos los Comentarios");
+        List<ComentarioEntity> editorials = persistence.findAll();
+        LOGGER.info("Termina proceso de consultar todos los Comentarios");
+        return editorials;
+    }
+      
+        public ComentarioEntity getComentarios(Long id) {
+        return persistence.find(id);
+    }
+        
+        public ComentarioEntity updateComentario(ComentarioEntity entity) throws BusinessLogicException  {
+        
+        return persistence.update(entity);
+    }
+        
+        public void deleteComentario(ComentarioEntity entity) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "Inicia proceso de borrar Comentario con id={0}", entity.getId());    
+        persistence.delete(entity.getId());
+        LOGGER.log(Level.INFO, "Termina proceso de borrar Comentario con id={0}", entity.getId());
+    }
+}
