6d502f74f95a4e56af94afed0c6e937831d71ce2
JULIAN CAMILO MONTOYA RODRIGUEZ <jc.montoyar@sis.virtual.uniandes.edu.co>
2018-04-28 21:45:16 -0500

    Caso de uso create plan hecho

--- turismo-web/src/main/webapp/src/modules/plans/new/plans.new.ctrl.js
+++ turismo-web/src/main/webapp/src/modules/plans/new/plans.new.ctrl.js
@@ -2,7 +2,7 @@
     var mod = ng.module("planModule");
     mod.constant("plansContext", "api/plans");
     mod.constant("preferenciasContext", "api/preferences");
-    mod.controller('planNewCtrl', ['$scope', '$http', 'plansContext', '$state', 'preferenciasContext', '$rootScope', '$filter',
+    mod.controller('planNewCtrl', ['$scope', '$http', 'plansContext', '$state', 'preferenciasContext', '$rootScope',
         /**
          * @ngdoc controller
          * @name plans.controller:planNewCtrl
@@ -18,89 +18,47 @@
          * @param {Object} $state Dependencia injectada en la que se recibe el 
          * estado actual de la navegación definida en el módulo.
          * @param {Object} preferenciasContext Constante injectada que contiene la ruta
-         * donde se encuentra el API de Libros en el Backend.
+         * donde se encuentra el API de preferencias en el Backend.
          * @param {Object} $rootScope Referencia injectada al Scope definida para
          * toda la aplicación.
          */
-        function ($scope, $http, plansContext, $state, preferenciasContext, $rootScope, $filter) {
+        function ($scope, $http, plansContext, $state, preferenciasContext, $rootScope) {
             $rootScope.edit = false;
 
-            $scope.data = {"categoriasPlan":[{tipoPlan: "familiar"}]};
-            
-            // Este arreglo guardara los ids de los preferencias asociados y por asociar al plan.
-            var nombresCat = [];
-            
-            // Este arreglo mostrará los preferencias una vez esten filtrados visualmente por lo que el plan ya tiene asociado.
-            $scope.allPreferenciasShow = [];
-            
-            
-            
+            $scope.data = {};          
+                      
             $http.get(preferenciasContext).then(function (response) {
-                    $scope.Allpreferencias = response.data;
-                    $scope.preferenciasAuthor = $scope.Allpreferencias;
-                    $scope.allPreferenciasShow = [];
-                    $scope.allPreferenciasAuthor = $scope.Allpreferencias;
-                    $scope.data.categoriasPlan = $scope.Allpreferencias;
-
-                });
-            //funciones para el drag and drop de HTML5 nativo
-            $scope.allowDrop = function (ev) {
-                ev.preventDefault();
-            };
-
-            $scope.drag = function (ev) {
-                ev.dataTransfer.setData("text", ev.target.id);
-            };
-
-            $scope.dropAdd = function (ev) {
-                ev.preventDefault();
-                var data = ev.dataTransfer.getData("text");
-                ev.target.appendChild(document.getElementById(data));
-                //Cuando un preferencia se añade al plan, se almacena su id en el array nombresCat
-                nombresCat.push("" + data);
-            };
-
-            $scope.dropDelete = function (ev) {
-                ev.preventDefault();
-                var data = ev.dataTransfer.getData("text");
-                ev.target.appendChild(document.getElementById(data));
-                //Para remover el preferencia que no se va asociar, por eso se usa el splice que quita el id del preferencia en el array nombresCat
-                var index = nombresCat.indexOf(data);
-                if (index > -1) {
-                    nombresCat.splice(index, 1);
-                }
-            };
-            
+                    $scope.AllPreferencias = response.data;
+                });           
             /**
              * @ngdoc function
              * @name createEditorial
              * @methodOf plans.controller:planNewCtrl
              * @description
-             * Esta función utiliza el protocolo HTTP para crear el plan.
-             * @param {Object} plan Objeto con el nuevo plan.
              */
             $scope.createPlan = function () {
-                $scope.addCategorias();
-               $scope.data.categoriasPlan = $scope.allpreferencias;
+                $scope.addCategorias($scope.data.categorias);
+               $scope.data.categoriasPlan = $scope.categoriasPlan;
                 $http.post(plansContext, $scope.data).then(function (response) {
                     $state.go('plansList', {planId: response.data.id}, {reload: true});
                 });
             };
-            
             /**
              * @ngdoc function
              * @name addCategorias
              * @methodOf authors.controller:authorUpdateCtrl
              * @description
              * Busca las nuevas categorias en el $scope.
+             * @param {Object} Un string con el nombre de las preferencias a asociar
              */
-            $scope.addCategorias = function () {
-                for (var ite in nombresCat) {
-                    for (var all in $scope.Allpreferencias) {
-                        if ($scope.Allpreferencias[all].id === parseInt(nombresCat[ite])) {
-                            $scope.allPreferenciasAuthor.push($scope.Allpreferencias[all]);
-                        }
-                    }
+            $scope.addCategorias = function (splitear) 
+            {
+                $scope.categoriasPlan = [];
+                var splited = splitear.split("-"), i;
+                for (i = 0; i < splited.length; i++)
+                {
+                    var categoria = {"tipoPlan":splited[i]};
+                   $scope.categoriasPlan.push(categoria); 
                 }
             };
         }
